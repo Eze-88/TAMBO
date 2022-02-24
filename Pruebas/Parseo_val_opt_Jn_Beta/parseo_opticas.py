6a7c48b320790f76860f541774d0faa6e9b8cd54
Eze-88 <ezequielap@gmail.com>
2022-02-24 10:54:25 -0300

    Prueba con el VSC

--- Pruebas/Parseo_val_opt_Jn_Beta/parseo_opticas.py
+++ Pruebas/Parseo_val_opt_Jn_Beta/parseo_opticas.py
@@ -12,7 +12,7 @@ with open(template) as f, open(output_txt) as f_2:  # abro los dos archivos, el 
     #Proceso el resultado y el header para convertir los array del resultado en diccionarios
     dict_result = [dict(zip(re_table.header, pr)) for pr in result]
 
-    i = 0
+    i = 0   # indice para contar los resultados obtenidos
     for x in dict_result:
         print(x["interface"] + " " + x["lane"]+ " " + x["amp"]+ " " + x["OutputPower"]+ " " + x["RxPower"]+ " " + x["grados"]+ " " + x["OutputHighWarning"]+ " " + x["OutputLowWarning"]+ " " + x["RxHighWarning"]+ " " + x["RxLowWarning"])
         i+=1
