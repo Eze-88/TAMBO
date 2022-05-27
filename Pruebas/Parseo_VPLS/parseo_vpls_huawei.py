77ed2af6634097c143febc01f8a7c23a69ed96c1
ezequiel <ezequielap@gmail.com>
2022-05-27 15:53:57 -0300

    Nombre VPLSs

--- Pruebas/Parseo_VPLS/parseo_vpls_huawei.py
+++ Pruebas/Parseo_VPLS/parseo_vpls_huawei.py
@@ -1,7 +1,8 @@
+from itertools import count
 import textfsm
 
-template = "c:/Users/u559950/Documents/Desarrollo y programacion/TAMBO/Pruebas/Parseo_val_opt_Jn_Beta/parseo_vty.fsm"
-output_txt = "c:/Users/u559950/Documents/Desarrollo y programacion/TAMBO/Pruebas/Parseo_val_opt_Jn_Beta/crudo_vty.txt"
+template = "c:/Users/ezequ/Repositorios_GIT/TAMBO/Pruebas/Parseo_VPLS/parseo_vpls_huawei.fsm"
+output_txt = "c:/Users/ezequ/Repositorios_GIT/TAMBO/Pruebas/Parseo_VPLS/crudo_huawei.txt"
 
 with open(template) as f, open(output_txt) as f_2:  # abro los dos archivos, el del template y el del output del equipo
     re_table = textfsm.TextFSM(f) # inicializo el parser con el template
@@ -10,8 +11,7 @@ with open(template) as f, open(output_txt) as f_2:  # abro los dos archivos, el 
     #Proceso el resultado y el header para convertir los array del resultado en diccionarios
     dict_result = [dict(zip(re_table.header, pr)) for pr in result]
 
-    print(dict_result)
-
-    # i = 0   # indice para contar los resultados obtenidos
-    # for x in dict_result:
-    #     print(x["line"] + " " + x["privilege"])
+    #print(dict_result)
+    for item in dict_result:
+        print(item)
+    print(len(dict_result))
