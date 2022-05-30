2e7ac1b6a3c52889993448cd0af6afc7395906dc
ezequiel <ezequielap@gmail.com>
2022-05-30 17:03:28 -0300

    Pruebas l2vc

--- Pruebas/Parseo_VPLS/parseo_vpls_huawei.py
+++ Pruebas/Parseo_VPLS/parseo_vpls_huawei.py
@@ -1,7 +1,8 @@
 from itertools import count
 import textfsm
 
-template = "c:/Users/ezequ/Repositorios_GIT/TAMBO/Pruebas/Parseo_VPLS/parseo_vpls_huawei.fsm"
+#template = "c:/Users/ezequ/Repositorios_GIT/TAMBO/Pruebas/Parseo_VPLS/parseo_vpls_huawei.fsm"
+template = "c:/Users/ezequ/Repositorios_GIT/TAMBO/Pruebas/Parseo_VPLS/parseo_l2vc_huawei.fsm"
 output_txt = "c:/Users/ezequ/Repositorios_GIT/TAMBO/Pruebas/Parseo_VPLS/crudo_huawei.txt"
 
 with open(template) as f, open(output_txt) as f_2:  # abro los dos archivos, el del template y el del output del equipo
@@ -11,9 +12,6 @@ with open(template) as f, open(output_txt) as f_2:  # abro los dos archivos, el 
     #Proceso el resultado y el header para convertir los array del resultado en diccionarios
     dict_result = [dict(zip(re_table.header, pr)) for pr in result]
 
-    for item in dict_result:
-        if item['tipo_interfaz'] == 'GE':
-            item['interfaz'] = 'GigabitEthernet'+item['interfaz']
     for item in dict_result:
         print(item)
     print(len(dict_result))
