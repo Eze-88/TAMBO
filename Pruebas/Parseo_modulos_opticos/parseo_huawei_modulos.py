1f79e4fe8d17b1f4fac3002c9eab411f0e3a94c3
ezequiel <ezequielap@gmail.com>
2022-06-10 16:57:35 -0300

    Sumado v5

--- Pruebas/Parseo_modulos_opticos/parseo_huawei_modulos.py
+++ Pruebas/Parseo_modulos_opticos/parseo_huawei_modulos.py
@@ -1,8 +1,8 @@
 import textfsm
 from tabulate import tabulate
 
-template = "c:/Users/ezequ/Repositorios_GIT/TAMBO/Pruebas/Parseo_modulos_opticos/parseo_huawei_opticos.fsm"
-output_txt = "c:/Users/ezequ/Repositorios_GIT/TAMBO/Pruebas/Parseo_modulos_opticos/crudo_huawei.txt"
+template = "c:/Users/ezequ/Repositorios_GIT/TAMBO/Pruebas/Parseo_modulos_opticos/parseo_huawei_opticos_v5.fsm"
+output_txt = "c:/Users/ezequ/Repositorios_GIT/TAMBO/Pruebas/Parseo_modulos_opticos/crudo_huawei_v5.txt"
 
 with open(template) as f, open(output_txt) as f_2:  # abro los dos archivos, el del template y el del output del equipo
     re_table = textfsm.TextFSM(f) # inicializo el parser con el template
