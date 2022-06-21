f11d8f1a8397d697d3bf5cfc90617f3552c5aba9
ezequiel <ezequielap@gmail.com>
2022-06-21 12:00:19 -0300

    Parseo rutas Cisco

---
+++ Pruebas/Rutas_RR/Parseador.py
@@ -0,0 +1,24 @@
+import textfsm
+from tabulate import tabulate
+
+template = "c:/Users/ezequ/Repositorios_GIT/TAMBO/Pruebas/Rutas_RR/parseador_rutas_cisco.fsm"
+output_txt = "c:/Users/ezequ/Repositorios_GIT/TAMBO/Pruebas/Rutas_RR/crudo_rutas_cisco.txt"
+
+with open(template) as f, open(output_txt) as f_2:  # abro los dos archivos, el del template y el del output del equipo
+    re_table = textfsm.TextFSM(f) # inicializo el parser con el template
+    result = re_table.ParseText(f_2.read()) # aplico el parser al texto del output del equipo
+
+    print(tabulate(result, headers=re_table.header))
+
+    #Proceso el resultado y el header para convertir los array del resultado en diccionarios
+    dict_result = [dict(zip(re_table.header, pr)) for pr in result]
+    print(len(dict_result))
+
+    #Para adaptar el nombre ficticio de la interfaz al util
+    # for item in dict_result:
+    #     if item['tipo_interfaz'] == 'GE':
+    #         item['interfaz'] = 'GigabitEthernet' + item['interfaz']
+
+    # for item in dict_result:
+    #     print(item)
+    # print(len(dict_result))
